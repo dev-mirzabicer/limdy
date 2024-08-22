c17155acb871213b855bd89f8590d715d46ba827
dev-mirzabicer <mirzabicer.dev@gmail.com>
2024-08-23 01:42:09 +0300

    add language enum

---
+++ include/core/limdy_types.h
@@ -0,0 +1,26 @@
+/**
+ * @file limdy_types.h
+ * @brief Global type definitions for the Limdy project.
+ *
+ * This file contains type definitions that are used across multiple
+ * components of the Limdy project.
+ *
+ * @author Mirza Bicer
+ * @date 2024-08-23
+ */
+
+#ifndef LIMDY_TYPES_H
+#define LIMDY_TYPES_H
+
+/**
+ * @brief Enumeration of supported languages.
+ */
+typedef enum
+{
+    LANG_ENGLISH,
+    LANG_SPANISH,
+    // Add more languages as needed
+    LANG_COUNT
+} Language;
+
+#endif // LIMDY_TYPES_H
